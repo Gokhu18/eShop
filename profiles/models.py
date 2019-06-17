db7b5c58022ac4b6d7812ef5aeaa4056232296b2
Mounica Paladugu <monica1291@gmail.com>
2019-06-16 18:53:22 -0700

    profiles model modifications

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,3 @@
-from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
@@ -7,6 +6,9 @@ from django.db import models
 class profile(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField(default='description default text')
+    location = models.CharField(
+        max_length=120, default='my location info', blank=True, null=True)
+    job = models.CharField(max_length=120, null=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
